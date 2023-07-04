64db88342edf0237d607a59bc79178cacc4b073d
carminoplata <fortekarma@gmail.com>
2023-07-04 22:11:11 +0200

    Updated Logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,8 +4,8 @@ import Nav from "react-bootstrap/Nav";
 import { ThemeContext } from "../../Context/theme";
 
 import Container from "react-bootstrap/Container";
-import logoLight from "../../Assets/logoLight.png";
-import logoDark from "../../Assets/logoDark.png";
+import logoLight from "../../Assets/logo_light_1024.png";
+import logoDark from "../../Assets/logo_dark_1024.png";
 
 import { Link } from "react-router-dom";
 import "./navbar.css";
